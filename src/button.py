1f060816ab8826fb5e865eda0d39e98bf5485911
jfrozas <j.frozas@udc.es>
2024-03-17 16:32:27 +0100

    Comentarios de boton y director

--- src/button.py
+++ src/button.py
@@ -1,5 +1,19 @@
 class Button():
+
+	"""
+	
+	Clase encargada de manejar los botones de los diferentes menus.
+	
+	"""
+
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+
+		"""
+		
+		Inicializa todos los parametros necesarios para la creacion del boton: fondo, posicion(x e y), texto, fuente, color y color de hover
+		
+		"""
+
 		self.image = image
 		self.x_pos = pos[0]
 		self.y_pos = pos[1]
@@ -13,16 +27,39 @@ class Button():
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
 	def update(self, screen):
+
+		"""
+		
+		Muestra el boton por pantalla haciendo un blit
+		
+		"""
+
 		if self.image is not None:
 			screen.blit(self.image, self.rect)
+		
+		# En caso de que no haya fondo
 		screen.blit(self.text, self.text_rect)
 
 	def checkForInput(self, position):
+
+		"""
+		
+		Comprueba que el click se realice en el boton comparando sus posiciones
+		
+		"""
+
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			return True
 		return False
 
 	def changeColor(self, position):
+
+		"""
+		
+		Cuando se pasa el raton por encima del boton, se le cambia el color a las letras al color de hover.
+		
+		"""
+		
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
 		else:
--- src/director.py
+++ src/director.py
@@ -13,16 +13,33 @@ from mainmenu import *
 from victory import *
 
 class Director:
+
+    """
+    
+    Clase encargada del loop principal del juego, y llamar a las clases correspondientes para el cargado de sprites/mecanicas correspondientes
+    
+    """
+        
     def __init__(self):
+
+        """
+        
+        Inicializa la clase con sus respectivos parametros
+        
+        """
+
+        # Variables respectivas a la zona 
         self.current_zone = Zone1(self)
         self.end_zone = False
 
+        # Crea la ventana, y un caption
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Los Secretos de FICrol")
-
+        
+        # Carga el mapa
         self.tmx_map = ResourceManager.load('bosque', type='map')
 
-        # groups
+        # Se crean los grupos
         self.all_sprites = AllSprites('bosque')
         self.colliders = pygame.sprite.Group()
         self.bullets = pygame.sprite.Group()
@@ -32,6 +49,7 @@ class Director:
         self.hearts = pygame.sprite.Group()
         self.coins = pygame.sprite.Group()
 
+        # Variables necesarias para funcionamiento
         self.player_death = False
         self.scroll = False
         self.shop = False
@@ -40,8 +58,17 @@ class Director:
         self.spriteList = []
 
     def run(self):
+
+        """
+        
+        Setupea el los niveles y los lanza en orden.
+
+        """
+
+        # Inicializa el reloj
         self.clock = pygame.time.Clock()
 
+        # Nivel del bosque
         self.level_setup('bosque')
         while self.current_zone != None:
             self.current_zone.setup()
@@ -49,6 +76,7 @@ class Director:
             self.tutorial = False
             self.current_zone = self.current_zone.next_zone()
 
+        # Niveles de tienda, pueblo, tienda otra vez y tumba
         for level in ['tienda', 'pueblo', 'tienda', 'tumba']:
             self.reset(level)
             self.shop = not self.shop
@@ -59,11 +87,15 @@ class Director:
                 self.current_zone.setup()
                 self.loop()
                 self.current_zone = self.current_zone.next_zone()
+        
+        # Mensaje de victoria
         victory(self)
 
+    # Set up del nivel.
     def level_setup(self, level, money=0, weapon='staff'):
         self.block = []
 
+        # Coge las layers del nivel correspondiente, crea sus sprites
         for layer, has_collisions in MAP_LAYERS[level]:
             temp_list = []
             groups = [self.all_sprites, self.colliders] if has_collisions else self.all_sprites
@@ -76,6 +108,8 @@ class Director:
             self.spriteList.append(temp_list)
 
         temp_list = []
+
+        # Crea el player
         for obj in self.get_map_layer("player"):
             if obj.name == "Player":
                 self.player = Player(
@@ -97,11 +131,15 @@ class Director:
                 )
                 temp_list.append(self.player)
 
+                # Crea la barra de vida y la pega al player
                 healthBar = HealthBar(0, WINDOW_HEIGHT-25, 250, 50, 10, self.healthBar)
                 self.player.attach(healthBar)
                 self.player.healthBar = healthBar
+        
+        # Lista para el dibujado
         self.spriteList.append(temp_list)
 
+        # Si no es tienda, musica distinta
         if not self.shop:
             pygame.mixer.music.unload()
             pygame.mixer.music.load(f"audio/{level}.mp3")
@@ -110,6 +148,13 @@ class Director:
 
     def reset(self, map):
 
+        """
+
+        Resetea el nivel anterior, y se prepara para cargar el nivel siguiente pasado por parametro
+
+        """
+
+        # Mata todos los sprites
         for sprite in self.all_sprites:
             sprite.kill()
             self.all_sprites.remove(sprite)
@@ -117,9 +162,10 @@ class Director:
         self.current_zone = Zone1(self)
         self.end_zone = False
 
+        # Carga el mapa
         self.tmx_map = ResourceManager.load(map, type='map')
 
-        # groups
+        # Grupos (Probablemente no necesario)
         self.all_sprites = AllSprites(map)
         self.colliders = pygame.sprite.Group()
         self.bullets = pygame.sprite.Group()
@@ -136,16 +182,28 @@ class Director:
 
 
     def loop(self):
+
+        """
+        
+        Loop principal de un nivel.
+        
+        """
+
         self.end_zone = False
         sound_death = False
         pygame.event.clear()
 
+        # Mientras que siga habiendo zonas
         while not self.end_zone:
+
+            # Si el jugador esta muerto
             if self.player_death:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         sys.exit()
+
+                    # Boton para la restauracion tras muerte
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         if RETURN_BUTTON.checkForInput(MENU_MOUSE_POS):
                             director = Director()
@@ -155,6 +213,11 @@ class Director:
                     pygame.mixer.music.load("audio/death.mp3")
                     pygame.mixer.music.play(0,0.0)
                     sound_death = True
+
+                # El siguiente codigo se utiliza para la creacion del menu post mortem
+                            
+                # Crea un difuminado negro de fondo, y escribe las palabras defeat en una font cargada
+                    
                 self.display_surface.fill("black")
                 self.all_sprites.shop_draw(self.spriteList, self.bullets, self.enemy, self.coins, self.hearts, self.weapons)
                 fill_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
@@ -162,29 +225,36 @@ class Director:
                 fill_surface.fill(fill_color)
                 self.display_surface.blit(fill_surface, (0,0))
                 
+                # Carga la fuente y displayea el texto derrota
                 font = ResourceManager.load('font', type='font', fontsize=100)
                 defeat_text = font.render("DERROTA", True, "White")
                 defeat_rect = defeat_text.get_rect(center=(400, 400))
                 self.display_surface.blit(defeat_text, defeat_rect)
-                
+
+                # Displayea los botones necesarios, y chequea si se clica
                 RETURN_BUTTON = Button(image=None, pos=(400, 500), 
                             text_input="VOLVER", font=ResourceManager.load('font', type='font', fontsize=20), base_color="#ffffff", hovering_color="gray")
                 MENU_MOUSE_POS = pygame.mouse.get_pos()
                 RETURN_BUTTON.changeColor(MENU_MOUSE_POS)
                 RETURN_BUTTON.update(self.display_surface)
                 
+            # Si el jugador no esta muerto
             else:
-                # event loop
+                
                 for event in pygame.event.get():
+
+                    # Evento de salida
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         sys.exit()
-                    
+                
+                # Si no hay enemigos
                 if self.current_zone.enemy_counter == 0:
                     for obj in self.block:
                         if obj.rect.right <= self.current_zone.right_border:
                             obj.kill()
                             self.all_sprites.remove(obj)
+                    # Si se da el caso del scroll, mueve todo hacia el lado correspondiente
                     if self.scroll:
                         self.all_sprites.internal_offset.x -= 100
                         if self.all_sprites.internal_offset.x % 800 == 0:
@@ -193,24 +263,29 @@ class Director:
                             self.scroll = False
                             break
 
+                # Delta times -> No depender de los FPS
                 dt = self.clock.tick() / 1000
         
-                # update groups
+                # Actualizar los grupos
                 self.all_sprites.update(dt)
                 self.bullet_collision()
 
-                # draw groups
+                # Dibujar los grupos
                 self.display_surface.fill("black")
                 self.all_sprites.shop_draw(self.spriteList, self.bullets, self.enemy, self.coins, self.hearts, self.weapons)
 
+                # Dibuja todas las barras de vida dentro del grupo de healthBar
                 for bar in self.healthBar:
                     bar.draw(self.display_surface, self.all_sprites.internal_offset.x)
                 
+                # Textos varios que aparecen en la pantalla 
                 font = ResourceManager.load('font', type='font', fontsize=15)
                 coin_text = font.render("x"+str(self.player.money), True, "White")
                 coin_rect = coin_text.get_rect(center=(30, 8))
                 self.display_surface.blit(coin_text, coin_rect)
                 self.display_surface.blit(ResourceManager.load("coin"), (0, 0))
+
+                # Texto para el precio de la tienda
                 if self.shop:
                     font = ResourceManager.load('menufont', type='font', fontsize=30)
                     shop_text = font.render("Todo a 5", True, "White")
@@ -220,6 +295,8 @@ class Director:
                     image_tutorial = ResourceManager.load("tutorialtienda")
                     image_rect = image_tutorial.get_rect(center=(400,730))
                     self.display_surface.blit(image_tutorial, image_rect)
+
+                # Texto para el ptutorial
                 if self.tutorial:
                     image_tutorial = ResourceManager.load("tutorial")
                     image_rect = image_tutorial.get_rect(center=(400,700))
@@ -246,10 +323,20 @@ class Director:
             self.player.damage(1)
 
     def start_scroll(self):
+        """
+        
+        Setupea el scroll a true si se cumplen las condiciones
+        
+        """
         if self.current_zone.enemy_counter == 0:
             self.scroll = True
 
     def get_map_layer(self, name):
+        """
+        
+        Coge la capa pasada por parametro de un mapa
+        
+        """
         return self.tmx_map.get_layer_by_name(name)
     
     def get_enemy_groups(self):
@@ -269,6 +356,13 @@ class Director:
 
 
 class AllSprites(pygame.sprite.Group):
+
+    """
+    
+    Carga todos los sprites
+
+    """
+
     def __init__(self, map):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
@@ -284,6 +378,7 @@ class AllSprites(pygame.sprite.Group):
         self.internal_surf = pygame.Surface(self.internal_surf_size, pygame.SRCALPHA)
         self.internal_offset = pygame.math.Vector2(0,0)
 
+        # Carga el background correspondiente (Tienda lo carga, pero no lo utiliza)
         match map:
             case 'bosque': self.bg_surf = ResourceManager.load('background_bosque')
             case 'tienda': self.bg_surf = ResourceManager.load('background_bosque')
@@ -293,19 +388,27 @@ class AllSprites(pygame.sprite.Group):
         self.bg_rect = self.bg_surf.get_rect(topleft=(0, 0))
 
     def shop_draw(self, list, bullets, enemies, coins, hearts, weapons):
+
+        """
+        
+        Funcion de dibujado por capas
+        
+        """
+
         self.internal_surf.fill("black")
 
         bg_offset = self.bg_rect.topleft + self.internal_offset
         self.internal_surf.blit(self.bg_surf, bg_offset)
 
-        # active elements
+        # Dibuja primero las capas del mapa de Tiled
         for layer in list:
             
             for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
                 if sprite in layer:
                     offset_pos = sprite.rect.topleft + self.internal_offset
                     self.internal_surf.blit(sprite.image, offset_pos)
-
+        
+        # A continuacion dibuja grupos como enemigos, balas, monedas, corazones y armas
         for sprite in bullets:
             offset_pos = sprite.rect.topleft + self.internal_offset
             self.internal_surf.blit(sprite.image, offset_pos)
@@ -329,4 +432,5 @@ class AllSprites(pygame.sprite.Group):
         scaled_surf = pygame.transform.scale(self.internal_surf, self.internal_surf_size * self.zoom_scale)
         scaled_rect = scaled_surf.get_rect(center=(self.half_w, self.half_h))
 
+        # Hace el blit de la pantalla
         self.display_surface.blit(scaled_surf, scaled_rect)
