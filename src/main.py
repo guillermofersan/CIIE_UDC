542c74e679041cbcf942474d0f788adcb4885bc1
jfrozas <j.frozas@udc.es>
2024-03-17 20:47:07 +0100

    a

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     """
     
-    Clase principal del programa.d
+    Clase principal del programa
 
     Desde esta se inicializa pygame, se crea un director, el cual sera el que se encargue de manejar el loop principal del juego, y se llama al menu principal. Tras acabar el juego, se cerraria pygame, y el programa finalizaria.
 
